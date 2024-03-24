58c0d2305c799d8ea1dac13aa3e5de7fe04ce232
Joel Sousa <joel30052000@gmail.com>
2024-03-24 22:03:13 +0000

    Create Course.jsx

---
+++ part2/Course information step6/components/Course.jsx
@@ -0,0 +1,18 @@
+
+const Course = ({course}) =>{
+    const parts = course.parts
+
+    return(
+        <div>
+            <h1>{course.name}</h1>
+            {parts.map(part => 
+            <p key={part.id}>
+                {part.name} {part.exercises}
+            </p>)}              
+        </div>
+        
+    )
+}
+
+
+export default Course
