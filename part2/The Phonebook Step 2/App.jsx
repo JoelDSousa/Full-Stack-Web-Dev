2b9bff536a53ffdc1a534ac5b24b09b85a7365b6
Joel Sousa <joel30052000@gmail.com>
2024-03-29 12:03:55 +0000

    Create App.jsx

---
+++ part2/The Phonebook Step 2/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import Note from './components/Note.jsx'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id:1 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event)=>{
+    event.preventDefault();
+    const newPerson ={
+      name:newName,
+      id: persons.length+1
+    };
+    for(let i=0; i<persons.length;i++){
+      if(persons[i].name===newPerson.name){
+        const message = newPerson.name +' is already added to phonebook';
+        console.log(message);
+        alert(message)
+        return;
+      }
+    }
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    console.log('button clicked', event.target);
+  }
+
+  const handleNewName = (event)=>{
+  console.log(event.target.value);
+  setNewName(event.target.value);
+
+}
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName} >
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit"  >add</button>
+        </div>
+      </form>
+      
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(
+          person =>
+          <Note key={person.id} note = {person}/>
+        )}
+      </ul>
+      
+    </div>
+  )
+}
+
+export default App
