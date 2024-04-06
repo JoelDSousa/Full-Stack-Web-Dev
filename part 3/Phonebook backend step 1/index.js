741b3ef401d9b44e4be3f044e2f9980e65669424
Joel Sousa <joel30052000@gmail.com>
2024-04-06 23:46:15 +0100

    Added files
    Added the files used to fulfill the specifications of  Phonebook backend step 1

---
+++ part 3/Phonebook backend step 1/index.js
@@ -0,0 +1,79 @@
+const http = require('http')
+const express = require('express')
+
+
+
+const app = express()
+
+
+let phoneBooks = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/phoneBooks', (request, response) => {
+  console.log('phoneBooks');
+  response.json(phoneBooks)
+})
+
+app.get('/api/phoneBooks/:id', (request, response) => {
+
+  const id = Number(request.params.id)
+  const phoneBook = phoneBooks.find(phoneBook => phoneBook.id === id)
+  if (phoneBook) {    
+    response.json(phoneBook)  
+  } else {    
+    response.statusMessage = "phoneBook id not found"
+    response.status(404).send("<h1> phoneBook id not found </h1>")  
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/phoneBooks/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phoneBooks = phoneBooks.filter(phoneBook => phoneBook.id !== id)
+  
+  response.status(204).end()
+})
+
+app.post('/api/phoneBooks', (request, response) => {  
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  const phoneBook = request.body  
+  const maxId = phoneBooks.length > 0
+    ? Math.max(...phoneBooks.map(n => n.id)) 
+    : 0
+  phoneBook.id = maxId+1
+  phoneBooks = phoneBooks.concat(phoneBook)
+  response.json(phoneBook)
+})
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
